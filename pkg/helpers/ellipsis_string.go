7edc6e6ad4687b335cf71b6df3f15c032259da77
Ståle Dahl <stalehd@gmail.com>
2022-03-14 12:16:45 +0100

    Check for length arg in ellipsisstring

--- pkg/helpers/ellipsis_string.go
+++ pkg/helpers/ellipsis_string.go
@@ -2,6 +2,9 @@ package helpers
 
 // EllipsisString returns a string no longer than n. If the string is truncated an ellipsis is added
 func EllipsisString(s string, n int) string {
+	if n <= 0 {
+		return s
+	}
 	if len(s) > n && len(s) > 3 {
 		return s[:n-3] + "..."
 	}
