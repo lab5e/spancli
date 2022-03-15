22b4ccc1eed4b6764fd2fdbaebf9b069aa8b5db6
Ståle Dahl <stalehd@gmail.com>
2022-03-15 16:59:39 +0100

    Add delete command to outputs

--- pkg/cmd/output/delete.go
+++ pkg/cmd/output/delete.go
@@ -1,4 +1,32 @@
 package output
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/lab5e/spancli/pkg/commonopt"
+	"github.com/lab5e/spancli/pkg/helpers"
+)
+
 type deleteOutput struct {
+	ID     commonopt.Collection
+	OID    oid
+	Prompt commonopt.NoPrompt
+}
+
+func (c *deleteOutput) Execute([]string) error {
+	if !c.Prompt.Check() {
+		return errors.New("aborted by user")
+	}
+
+	client, ctx, done := helpers.NewSpanAPIClient()
+	defer done()
+
+	o, res, err := client.OutputsApi.DeleteOutput(ctx, c.ID.CollectionID, c.OID.OutputID).Execute()
+	if err != nil {
+		return helpers.ApiError(res, err)
+	}
+
+	fmt.Printf("Removed output %s\n", o.GetOutputId())
+	return nil
 }
