c0033824a771d4bf9d8ee2b65a053e6d2daf5d04
Ståle Dahl <stalehd@gmail.com>
2022-03-17 12:54:22 +0100

    Use table and default format options when dumping device

--- pkg/cmd/device/get.go
+++ pkg/cmd/device/get.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/lab5e/spancli/pkg/commonopt"
 	"github.com/lab5e/spancli/pkg/helpers"
 )
 
 type getDevice struct {
-	ID commonopt.CollectionAndDevice
+	ID     commonopt.CollectionAndDevice
+	Format commonopt.ListFormat
 }
 
 func (r *getDevice) Execute([]string) error {
@@ -21,11 +23,59 @@ func (r *getDevice) Execute([]string) error {
 		return helpers.ApiError(res, err)
 	}
 
-	jsonData, err := json.MarshalIndent(device, "", "  ")
+	t := helpers.NewTableOutput(r.Format)
+	t.SetTitle("Device %s", device.GetDeviceId())
+
+	t.AppendHeader(table.Row{"Field", "Value"})
+	// Dump as name.value list by converting to JSON and then back to map[string]any
+	buf, err := json.Marshal(device)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(string(jsonData))
+	nameValue := map[string]any{}
+	if err := json.Unmarshal(buf, &nameValue); err != nil {
+		return err
+	}
+	dumpFields(t, "", nameValue)
+
+	certs, res, err := client.DevicesApi.DeviceCertificate(ctx, r.ID.CollectionID, r.ID.DeviceID).Execute()
+	if err != nil {
+		return helpers.ApiError(res, err)
+	}
+
+	// List certificates separately
+	for i, v := range certs.Certificates {
+		t.AppendRow(table.Row{
+			fmt.Sprintf("device.certificate.%d.serial", i),
+			v.GetCertificateSerial(),
+		})
+		t.AppendRow(table.Row{
+			fmt.Sprintf("device.certificate.%d.expires", i),
+			helpers.DateFormat(v.GetExpires(), r.Format.NumericDate),
+		})
+	}
+	helpers.RenderTable(t, r.Format.Format)
 	return nil
+}
+
+func fieldName(prefix string, field string) string {
+	if prefix == "" {
+		return field
+	}
+	return prefix + "." + field
+}
+
+func dumpFields(t table.Writer, prefix string, nameValue map[string]any) {
+	for k, v := range nameValue {
+		subType, ok := v.(map[string]any)
+		if ok {
+			dumpFields(t, fieldName(prefix, k), subType)
+			continue
+		}
+		t.AppendRow(table.Row{
+			fieldName(prefix, k),
+			v,
+		})
+	}
 }
