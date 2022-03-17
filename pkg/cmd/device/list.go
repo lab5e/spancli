b03921de499a3207b822905e864f88210bb9ee69
Ståle Dahl <stalehd@gmail.com>
2022-03-17 12:54:06 +0100

    Change fields in device list command

--- pkg/cmd/device/list.go
+++ pkg/cmd/device/list.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/lab5e/go-spanapi/v4"
 	"github.com/lab5e/spancli/pkg/commonopt"
 	"github.com/lab5e/spancli/pkg/helpers"
 )
@@ -39,36 +40,70 @@ func (r *listDevices) Execute([]string) error {
 
 	t := helpers.NewTableOutput(r.Format)
 	t.SetTitle("Devices in %s", r.ID.CollectionID)
-	t.AppendHeader(table.Row{"DeviceID", "Name", "Last conn", "FW", "IMSI", "IMEI", "Tags"})
+	t.AppendHeader(table.Row{
+		"ID",
+		"Config",
+		"Network",
+		"IP",
+		"Last update",
+		"Tags"})
 
 	for _, device := range resp.Devices {
-		// only truncate name if we output as 'text'
-		name := device.GetTags()["name"]
-		if r.Format.Format == "text" {
-			name = helpers.EllipsisString(name, 25)
-		}
-
-		allocatedAt := "-"
-		if *device.Network.AllocatedAt != "0" {
-			allocatedAt = helpers.LocalTimeFormat(*device.Network.AllocatedAt)
-		}
-
-		fwVersion := "-"
-		if *device.Firmware.FirmwareVersion != "" {
-			fwVersion = *device.Firmware.FirmwareVersion
-		}
-
 		t.AppendRow(table.Row{
 			device.GetDeviceId(),
-			name,
-			allocatedAt,
-			fwVersion,
-			device.GetImsi(),
-			device.GetImei(),
+			getConfig(device),
+			getMetadata(device),
+			getIP(device),
+			getLastUpdate(device),
 			helpers.TagsToString(device.GetTags()),
 		})
 	}
 	helpers.RenderTable(t, r.Format.Format)
-
 	return nil
+}
+func getIP(d spanapi.Device) string {
+	if d.Metadata.Ciot != nil {
+		return d.Metadata.Ciot.GetAllocatedIp()
+	}
+	if d.Metadata.Inet != nil {
+		return d.Metadata.Inet.GetRemoteAddress()
+	}
+	return "-"
+}
+func getLastUpdate(d spanapi.Device) string {
+	if d.Metadata.Ciot != nil {
+		return d.Metadata.Ciot.GetAllocatedAt()
+	}
+	if d.Metadata.Inet != nil {
+		return d.Metadata.Inet.GetLastUpdate()
+	}
+	return "-"
+}
+
+func getConfig(d spanapi.Device) string {
+	if d.Config == nil {
+		return ""
+	}
+	if d.Config.HasCiot() && d.Config.Ciot.GetImsi() != "" {
+		return fmt.Sprintf("imsi:%s imei:%s", d.Config.Ciot.GetImsi(), d.Config.Ciot.GetImei())
+	}
+	// No config for inet devices
+	return ""
+}
+
+func getMetadata(d spanapi.Device) string {
+	if !d.HasMetadata() {
+		return "(no metadata)"
+	}
+	if d.Metadata.Ciot != nil {
+		operator := d.Metadata.SimOperator
+		if operator == nil {
+			operator = &spanapi.NetworkOperator{}
+		}
+		return fmt.Sprintf("cell:%s  operator:%s  country:%s", d.Metadata.Ciot.GetCellId(), operator.GetNetwork(), operator.GetCountry())
+	}
+	if d.Metadata.Inet != nil {
+		return fmt.Sprintf("certificate s/n:%s", d.Metadata.Inet.GetCertificateSerial())
+	}
+	return ""
 }
