31127625e8a9a70ec34243b551e6ac3441cfc861
Bjorn Borud <borud@lab5e.com>
2023-04-25 16:07:39 +0200

    remove required constraint from Token parameter

--- pkg/global/globals.go
+++ pkg/global/globals.go
@@ -7,9 +7,9 @@ import "time"
 
 // Parameters is the global parameters for all the commands
 type Parameters struct {
-	Token                string        `long:"token" env:"SPAN_API_TOKEN" description:"span API token" required:"yes"`
-	OverrideEndpoint     string        `long:"endpoint" env:"SPAN_API_ENDPOINT" description:"span endpoint override" required:"no"`
-	MQTTOverrideEndpoint string        `long:"mqtt-endpoint" env:"SPAN_MQTT_ENDPOINT" description:"span MQTT endpoint override" required:"no"`
+	Token                string        `long:"token" env:"SPAN_API_TOKEN" description:"span API token"`
+	OverrideEndpoint     string        `long:"endpoint" env:"SPAN_API_ENDPOINT" description:"span endpoint override"`
+	MQTTOverrideEndpoint string        `long:"mqtt-endpoint" env:"SPAN_MQTT_ENDPOINT" description:"span MQTT endpoint override"`
 	Timeout              time.Duration `long:"timeout" default:"15s" description:"timeout for operation"`
 	Debug                bool          `long:"debug" description:"turn on debug output"`
 }
