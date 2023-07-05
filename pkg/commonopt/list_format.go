46e1b1a3b412095cccae5ddd10364bcd427821a1
Bjorn Borud <borud@lab5e.com>
2023-07-05 12:56:46 +0200

    remove formats we do not use

--- pkg/commonopt/list_format.go
+++ pkg/commonopt/list_format.go
@@ -3,7 +3,7 @@ package commonopt
 // ListFormat holds common options for list formatting
 type ListFormat struct {
 	//lint:ignore SA5008 Multiple choices makes the linter unhappy
-	Format          string `long:"format" default:"text" description:"which output format to use" choice:"csv" choice:"html" choice:"markdown" choice:"text" choice:"json"`
+	Format          string `long:"format" default:"text" description:"which output format to use" choice:"csv" choice:"html" choice:"markdown" choice:"text"`
 	NoColor         bool   `long:"no-color" env:"SPAN_NO_COLOR" description:"turn off coloring"`
 	PageSize        int    `long:"page-size" description:"if set, chop output into pages of page-size length"`
 	MaxPayloadWdith int    `long:"payload-width" description:"maximum width of payload" default:"50"`
